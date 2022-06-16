a673f569a2c8a358e6478d516f32257278f047e6
jerry <jerry@gmail.com>
2022-06-17 00:07:47 +0800

    完成新版pm25網址更新

---
+++ main.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template
+from datetime import datetime
+import json
+from pm25 import get_pm25
+
+app = Flask(__name__)
+
+
+@app.route('/pm25/<sort>')
+@app.route('/pm25')
+def pm25(sort=None):
+    columns, values, error = get_pm25(sort)
+    time = get_today()
+    return render_template('./pm25.html', **locals())
+
+
+@app.route('/test')
+def test():
+    return render_template('./test.html')
+
+
+@app.route('/')
+@app.route('/<name>')
+def index(name='GUEST'):
+    time = get_today()
+    result = {'time': time, 'name': name}
+    return render_template('./index.html',
+                           result=result)
+
+
+@app.route('/stock')
+def stock():
+    time = get_today()
+    stocks = [
+        {'分類': '日經指數', '指數': '22,920.30'},
+        {'分類': '韓國綜合', '指數': '2,304.59'},
+        {'分類': '香港恆生', '指數': '25,083.71'},
+        {'分類': '上海綜合', '指數': '3,380.68'}
+    ]
+
+    return render_template('./stock.html', **locals())
+    # return json.dumps(stocks, ensure_ascii=False)
+
+
+@app.route('/sum/x=<x>&y=<y>')
+def get_sum(x, y):
+    try:
+        return f'總合為:{eval(x)+eval(y)}'
+    except Exception as e:
+        return e
+
+
+@app.route('/book/page=<int:page>')
+def get_page(page):
+    if page == 1:
+        return 'JAVA'
+    elif page == 2:
+        return 'PYTHON'
+    elif page == 3:
+        return 'C++'
+
+    return 'NO BOOK!'
+
+
+@app.route('/bmi/height=<height>&weight=<weight>')
+def get_bmi(height, weight):
+    try:
+        return f'''
+        身高:{height}cm 體重{weight}kg==>
+        BMI:{round(eval(weight)/(eval(height)/100)**2, 2)}'''
+    except Exception as e:
+        return '<h2>數值傳入錯誤!</h2>'
+
+
+def get_today():
+    today = datetime.now()
+    return today.strftime('%Y-%m-%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ pm25.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def get_pm25(sort=None):
+    print(sort)
+    columns, values, error = None, None, None
+    try:
+        print('資料解析中...')
+        url = 'https://sta.ci.taiwan.gov.tw/STA_AirQuality_v2/v1.0/Datastreams?$expand=Thing,Observations($orderby=phenomenonTime%20desc;$top=1)&$filter=name%20eq%20%27PM2.5%27%20and%20Thing/properties/authority%20eq%20%27%E8%A1%8C%E6%94%BF%E9%99%A2%E7%92%B0%E5%A2%83%E4%BF%9D%E8%AD%B7%E7%BD%B2%27%20and%20substringof(%27%E7%A9%BA%E6%B0%A3%E5%93%81%E8%B3%AA%E6%B8%AC%E7%AB%99%27,Thing/name)&$count=true'
+        datas = pd.read_json(url)['value'].to_list()
+
+        values = []
+        for data in datas:
+            city, stationName = data['Thing']['properties']['city'], data['Thing']['properties']['stationName']
+            resultTime, result = data['Observations'][0]['resultTime'], data['Observations'][0]['result']
+            # print(city, stationName, resultTime, result)
+
+            values.append(
+                (city, stationName, pd.to_datetime(resultTime).strftime('%Y-%m-%d %H:%M:%S'), result))
+
+        if sort:
+            values=sorted(values,key=lambda x:x[-1],reverse=True)
+            
+        columns = ['city', 'stationName', 'resultTime', 'result']
+
+    except Exception as e:
+        error = e
+
+    return columns, values, error
